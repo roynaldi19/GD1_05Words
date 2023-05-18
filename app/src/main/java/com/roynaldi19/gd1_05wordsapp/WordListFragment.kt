73c46d01fc1735b62b31837b0eebb72f33b406c4
roynaldi19 <roynaldi19@gmail.com>
2023-05-18 20:40:01 +0700

    update 23

--- app/src/main/java/com/roynaldi19/gd1_05wordsapp/WordListFragment.kt
+++ app/src/main/java/com/roynaldi19/gd1_05wordsapp/WordListFragment.kt
@@ -24,25 +24,14 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.roynaldi19.gd1_05wordsapp.databinding.FragmentWordListBinding
 
-/**
- * Displays a [RecyclerView] of words with search buttons to look them up.
- */
 class WordListFragment : Fragment() {
 
-    /**
-     * Provides global access to these variables from anywhere in the app
-     * via DetailListFragment.<variable> without needing to create
-     * a DetailListFragment instance.
-     */
     companion object {
         val LETTER = "letter"
         val SEARCH_PREFIX = "https://www.google.com/search?q="
     }
 
     private var _binding: FragmentWordListBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     private lateinit var letterId: String
@@ -50,7 +39,6 @@ class WordListFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Retrieve the LETTER from the Fragment arguments
         arguments?.let {
             letterId = it.getString(LETTER).toString()
         }
@@ -61,7 +49,6 @@ class WordListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Retrieve and inflate the layout for this fragment
         _binding = FragmentWordListBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -71,15 +58,11 @@ class WordListFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = WordAdapter(letterId, requireContext())
 
-        // Adds a [DividerItemDecoration] between items
         recyclerView.addItemDecoration(
             DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
         )
     }
 
-    /**
-     * Frees the binding object when the Fragment is destroyed.
-     */
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
